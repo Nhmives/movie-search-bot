9e2b95765054a2908cd39e22aa518d40ad9d32b3
Nhmives <nhmovies9@gmail.com>
2025-03-09 01:22:19 +0530

    Create movies_db.py

---
+++ movies_db.py
@@ -0,0 +1,13 @@
+# movies_db.py
+
+MOVIES = {
+    "inception": {
+        "title": "Inception",
+        "download_link": "http://example.com/inception.mp4"
+    },
+    "avatar": {
+        "title": "Avatar",
+        "download_link": "http://example.com/avatar.mp4"
+    },
+    # Add more movies as needed...
+}
